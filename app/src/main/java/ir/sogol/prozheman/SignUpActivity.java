2c1146668a1e723630938b8f813af754151bfd42
Sogol <sogoli249@gmail.com>
2021-06-15 15:55:01 +0430

    prozhe4

--- app/src/main/java/ir/sogol/prozheman/SignUpActivity.java
+++ app/src/main/java/ir/sogol/prozheman/SignUpActivity.java
@@ -2,9 +2,17 @@ package ir.sogol.prozheman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,8 +20,30 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
 
+        Button btnClick = (Button) findViewById(R.id.btnSignUpSabtNum);
+        TextView btnGoToSignIn = (TextView) findViewById(R.id.txtGoToSignIn);
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
 
+
+
+            }
+        });
+
+
+        btnGoToSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(getApplicationContext() , SignInActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
 
 
